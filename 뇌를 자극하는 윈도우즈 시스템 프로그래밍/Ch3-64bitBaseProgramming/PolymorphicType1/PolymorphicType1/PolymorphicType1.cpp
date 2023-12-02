50b22e9019c4fd8b7525a27ef7940f74c5f86ccc
Sevenshards <sevenshards@hanyang.ac.kr>
2023-12-02 17:56:52 +0900

    32비트 환경에서 실행할 경우의 코드 추가

--- 뇌를 자극하는 윈도우즈 시스템 프로그래밍/Ch3-64bitBaseProgramming/PolymorphicType1/PolymorphicType1/PolymorphicType1.cpp
+++ 뇌를 자극하는 윈도우즈 시스템 프로그래밍/Ch3-64bitBaseProgramming/PolymorphicType1/PolymorphicType1/PolymorphicType1.cpp
@@ -13,10 +13,16 @@
 #include <tchar.h>
 #include <Windows.h>
 
-// 메모리 공간상 거리 차이를 계산하기 위한 함수
+// 메모리 공간상 거리 차이를 계산하기 위한 함수 (x64, 64비트)
 UINT64 CalcDistance(UINT64 a, UINT64 b)
 {
 	return b - a;
+}
+
+// 메모리 공간상 거리 차이를 계산하기 위한 함수 (x86, 32비트)
+UINT CalcDistance(UINT a, UINT b)
+{
+	return a - b;
 }
 
 int _tmain()
@@ -24,6 +30,11 @@ int _tmain()
 	INT val1 = 10;
 	INT val2 = 20;
 
+	// x86, 32비트 환경
+	_tprintf(_T("Position %u, %u \n"), (UINT)&val1, (UINT)&val2);
+	_tprintf(_T("Distance: %u \n"), CalcDistance((UINT)&val1, (UINT)&val2));
+
+	// x64, 64비트 환경
 	_tprintf(_T("Position %llu, %llu \n"), (UINT64)&val1, (UINT64)&val2);
 	_tprintf(_T("Distance: %llu \n"), CalcDistance((UINT64)&val1, (UINT64)&val2));
 
