40e42fabfdaf63ebdf42219904b211f585676033
Sevenshards <sevenshards@hanyang.ac.kr>
2023-12-03 19:42:15 +0900

    명령 프롬프트 프로젝트 2단계 구현

--- 뇌를 자극하는 윈도우즈 시스템 프로그래밍/CMDPromptProject/CMDPromptProject/CMDPrompt.cpp
+++ 뇌를 자극하는 윈도우즈 시스템 프로그래밍/CMDPromptProject/CMDPromptProject/CMDPrompt.cpp
@@ -1,12 +1,12 @@
 /*
 * Windows System Programming - 명령 프롬프트(CMD Prompt) 프로젝트
 * 파일명: CMDPrompt.cpp
-* 파일 버전: 0.1
+* 파일 버전: 0.2
 * 작성자: Sevenshards
-* 작성 일자: 2023-12-02
-* 이전 버전 작성 일자:
-* 버전 내용: 명령 프롬프트의 골격 작성
-* 이전 버전 내용:
+* 작성 일자: 2023-12-03
+* 이전 버전 작성 일자: 2023-12-02
+* 버전 내용: 명령 프롬프트 기능 추가(프로그램 실행)
+* 이전 버전 내용: 명령 프롬프트의 골격 작성
 */
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -19,6 +19,7 @@
 
 #define STR_LEN 256
 #define CMD_TOKEN_NUM 10
+#define DIR_LEN MAX_PATH+1
 
 // 오류 처리를 위한 전역변수 문자열 선언
 TCHAR ERROR_CMD[] = _T("'%s'는 실행할 수 있는 프로그램이 아닙니다.\n");
@@ -45,6 +46,11 @@ int _tmain()
 }
 
 TCHAR cmdString[STR_LEN];
+// 이 부분에 대한 정리를 안해서 개념이 헷갈렸었다.
+// 명령어는 하나만 들어오는 것이 아니라 실행할 프로그램 뒤에 옵션이 붙는 경우들이 있다.
+// 그런 것들을 고려해서 2차원 배열의 형태로 만든 것.
+// 그래서 cmdTokenList[0]은 실행할 파일의 이름이 되는 것이다.
+// main에서의 argv로 생각하면 argv[1]이 여기서는 cmdTokenList[0]인 것이다.
 TCHAR cmdTokenList[CMD_TOKEN_NUM][STR_LEN];
 // 받아들인 명령어를 토큰으로 쪼갤 때 사용할 구분 기준
 // 공백문자(' '), 콤마(','), 탭('\t'), 개행('\n')
@@ -85,14 +91,28 @@ int CmdProcessing(void)
 	}
 	else if (!_tcscmp(cmdTokenList[0], _T("추가되는 명령어 1")))
 	{
-
+		
 	}
 	else if (!_tcscmp(cmdTokenList[0], _T("추가되는 명령어 2")))
 	{
+
 	}
 	else
 	{
-		_tprintf(ERROR_CMD, cmdTokenList[0]);
+		STARTUPINFO si = { 0, }; // STARTUPINFO 구조체를 0으로 초기화
+		PROCESS_INFORMATION pi; // PROCESS_INFORMATION 구조체
+		BOOL state; // 프로세스 생성 성공 여부를 확인하기 위해 사용한 변수
+
+		si.cb = sizeof(si); // STARTUPINFO 구조체의 크기 초기화
+
+		state = CreateProcess(
+			NULL, cmdTokenList[0], NULL, NULL,
+			TRUE, NULL,	NULL, NULL, &si, &pi
+		);
+
+		if (state == 0) // 프로세스 생성이 실패했다면
+			_tprintf(ERROR_CMD, cmdTokenList[0]);
+		
 	}
 
 	return 0;
