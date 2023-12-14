af93591af627241b23a762ba95975d902ea11a7e
Sevenshards <sevenshards@hanyang.ac.kr>
2023-12-15 00:56:59 +0900

    DLL 활용 예제 - 계산기

---
+++ 뇌를 자극하는 윈도우즈 시스템 프로그래밍/Ch21-DynamicLinkingLibrary(DLL)/Calculator/Calculator/Calculator.cpp
@@ -0,0 +1,34 @@
+﻿/*
+* Windows System Programming - Dynamic Linking Library (DLL)
+* 파일명: Calculator.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2023-12-15
+* 이전 버전 작성 일자:
+* 버전 내용: DLL 활용 예제 - 계산기
+* 이전 버전 내용:
+*/
+#include "pch.h"
+#define _COMPLING_DLL_CALCULATOR
+#include "Calculator.h"
+
+
+LIBSPEC int Add(int a, int b)
+{
+	return a + b;
+}
+
+LIBSPEC int Min(int a, int b)
+{
+	return a - b;
+}
+
+LIBSPEC double Div(double a, double b)
+{
+	return a / b;
+}
+
+LIBSPEC double Mul(double a, double b)
+{
+	return a * b;
+}
