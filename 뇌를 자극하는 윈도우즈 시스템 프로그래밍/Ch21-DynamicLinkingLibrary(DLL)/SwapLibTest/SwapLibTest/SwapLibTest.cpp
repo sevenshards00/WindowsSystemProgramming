d4eceaec5dc0b5305675a5be24a4b76916519f29
Sevenshards <sevenshards@hanyang.ac.kr>
2023-12-14 23:51:30 +0900

    정적 라이브러리 기본 예제

---
+++ 뇌를 자극하는 윈도우즈 시스템 프로그래밍/Ch21-DynamicLinkingLibrary(DLL)/SwapLibTest/SwapLibTest/SwapLibTest.cpp
@@ -0,0 +1,28 @@
+﻿/*
+* Windows System Programming - Dynamic Linking Library (DLL)
+* 파일명: MemoryMappedFileSort.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2023-12-14
+* 이전 버전 작성 일자:
+* 버전 내용: 정적 라이브러리 기본 예제
+* 이전 버전 내용:
+*/
+
+#include <stdio.h>
+#include <tchar.h>
+#include <Windows.h>
+#include "Swap.h"
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	int a = 10;
+	int b = 20;
+	_tprintf(TEXT("Before: %d, %d \n"), a, b);
+
+	swap(&a, &b);
+
+	_tprintf(TEXT("After: %d, %d\n"), a, b);
+
+	return 0;
+}
---
+++ 뇌를 자극하는 윈도우즈 시스템 프로그래밍/Ch21-DynamicLinkingLibrary(DLL)/SwapStaticLib/SwabStaticLib/Swap.cpp
@@ -0,0 +1,19 @@
+﻿/*
+* Windows System Programming - Dynamic Linking Library (DLL)
+* 파일명: Swap.h
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2023-12-14
+* 이전 버전 작성 일자:
+* 버전 내용: 정적 라이브러리 기본 예제
+* 이전 버전 내용:
+*/
+
+#include "pch.h"
+
+void swap(int* v1, int* v2)
+{
+	int temp = *v1;
+	*v1 = *v2;
+	*v2 = temp;
+}
