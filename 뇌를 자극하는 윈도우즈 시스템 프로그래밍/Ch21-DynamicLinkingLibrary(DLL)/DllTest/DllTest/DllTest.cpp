8eb080eb0c90718ffd6f0ce8cd909c3b99331389
Sevenshards <sevenshards@hanyang.ac.kr>
2023-12-15 00:19:11 +0900

    동적 라이브러리 기본 예제

---
+++ 뇌를 자극하는 윈도우즈 시스템 프로그래밍/Ch21-DynamicLinkingLibrary(DLL)/DllTest/DllTest/DllTest.cpp
@@ -0,0 +1,30 @@
+﻿/*
+* Windows System Programming - Dynamic Linking Library (DLL)
+* 파일명: DllTest.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2023-12-14
+* 이전 버전 작성 일자:
+* 버전 내용: 동적 라이브러리 기본 예제
+* 이전 버전 내용:
+*/
+
+#pragma comment(lib, "SwapDll.lib")
+
+#include <stdio.h>
+#include <tchar.h>
+#include <Windows.h>
+#include "SwapDll.h"
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	int a = 10;
+	int b = 20;
+	_tprintf(TEXT("Before: %d, %d \n"), a, b);
+
+	swap(&a, &b);
+
+	_tprintf(TEXT("After: %d, %d\n"), a, b);
+
+	return 0;
+}
